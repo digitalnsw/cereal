831b642d98262ddd04a5fb3acf232db2cff4df24
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-27 12:28:29 +1300

    Added query.js

---
+++ docs/js/query.js
@@ -0,0 +1,8 @@
+
+$( document ).ready(function() {
+  $("button").click(function(){
+    $.ajax({url: "demo_test.txt", success: function(result){
+      $("#div1").html(result);
+    }});
+  });
+});

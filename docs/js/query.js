239add2430ea9cdaf76550cc7e00d207d03a15c4
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-27 14:54:35 +1300

    Reads the medicare check box

--- docs/js/query.js
+++ docs/js/query.js
@@ -5,41 +5,49 @@
 
 $( document ).ready(function() {
 
-  var data = {
-    "persons": {
-       "parent1":{
-          "is_guardian": {
-              "2019-02": true
-          },
-          "active_kids__is_eligible": {"2019-02": null}
-      },
-      "child1": {
-          "is_nsw_resident": {"2019-02":true},
-          "is_enrolled_in_school": {"2019-02": true},
-          "birth": {"ETERNITY": "2014-01-01"},
-          "active_kids__child_meets_criteria": {"2019-02": null},
-          "active_kids__voucher_amount": {"2019-02": "100"},
-          "has_valid_medicare_card": {"2019-02": true}
-      }
-    },
-    "families": {
-        "family1": {
-            "parents": ["parent1"],
-            "children": ["child1"]
-        }
-    }
-  
-  };
 
   $("button").click(function(){
-    $('#response').text('Fetching....');
-    $.ajax({
-      url: "https://openfisca-nsw-staging.herokuapp.com/calculate",
-      data : JSON.stringify(data),
-      method: 'POST',
-      contentType: 'application/json',
-      success: function(result){
-        $("#response").text(JSON.stringify(result, null, '\t'));
-    }});
+
+    var child_has_medicare = $('#child_has_medicare-0').is(':checked');
+    
+    var data = {
+      "persons": {
+         "parent1":{
+            "is_guardian": {
+                "2019-02": true
+            },
+            "active_kids__is_eligible": {"2019-02": null}
+        },
+        "child1": {
+            "is_nsw_resident": {"2019-02": true},
+            "is_enrolled_in_school": {"2019-02": true},
+            "birth": {"ETERNITY": "2014-01-01"},
+            "active_kids__child_meets_criteria": {"2019-02": null},
+            "active_kids__voucher_amount": {"2019-02": "100"},
+            "has_valid_medicare_card": {"2019-02": child_has_medicare}
+        }
+      },
+      "families": {
+          "family1": {
+              "parents": ["parent1"],
+              "children": ["child1"]
+          }
+      }
+    
+    };
+
+
+    $("#response").text(JSON.stringify(data, null, '\t'));
+    
+    // $('#response').text('Fetching....');
+
+    // $.ajax({
+    //   url: "https://openfisca-nsw-staging.herokuapp.com/calculate",
+    //   data : JSON.stringify(data),
+    //   method: 'POST',
+    //   contentType: 'application/json',
+    //   success: function(result){
+    //     $("#response").text(JSON.stringify(result, null, '\t'));
+    // }});
   });
 });
